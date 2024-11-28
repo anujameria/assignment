1f9ed7ad6c093552abe71bc5337b73bc174a9d4e
anuj ameria <sainanuj28@gmail.com>
2024-11-28 11:33:23 +0530

    variables

--- Variable/variable.js
+++ Variable/variable.js
@@ -1,7 +1,7 @@
 let a = 50;
 const b =40;
 // let value change but const not change after declaretion
-// let ek function and block scope variable hai
+// let ek function and block scope variable hai jo apne scope se bahar acces nahi hota
 
 console.log(a);
 console.log(b);
