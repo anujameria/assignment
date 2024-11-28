a7facba4667cabe424153322bfbef007538cf370
anuj ameria <sainanuj28@gmail.com>
2024-11-28 12:09:24 +0530

    dataTypes

---
+++ DataTypes/datatypes_operation.js
@@ -0,0 +1,13 @@
+let a= "anuj"
+let b = " sain"
+let c = a + b
+console.log(c);//anuj sain
+
+console.log(1+2+3);//6
+console.log("1"+2+3);//123
+console.log(1+2+"3");//33
+
+//jo opertion phle perform hoga output uske according aayega..
+
+
+
