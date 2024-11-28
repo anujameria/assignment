bbc943d0a38832780c47c39acdc1551d4b71c7a8
anuj ameria <sainanuj28@gmail.com>
2024-11-28 11:48:35 +0530

    conversion_datatypes

---
+++ DataTypes/conversion.js
@@ -0,0 +1,23 @@
+//covert datatypes to another datatypes
+
+let name="anuj"
+
+let changeName=Number(name)
+console.log(changeName); //NaN
+
+let number=40;
+
+let changeNumber=String(number)
+console.log(changeNumber);
+console.log(typeof changeNumber);//string
+
+
+let love= 1
+
+let trueLove=Boolean(love)
+console.log(trueLove); //true
+console.log(typeof trueLove);
+
+
+
+
