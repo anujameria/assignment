b7e5f4f429e4becd244f02f76a5d4b9963b03d44
anuj ameria <sainanuj28@gmail.com>
2024-11-28 13:00:45 +0530

    datatypes

---
+++ DataTypes/datatypes_comparison.js
@@ -0,0 +1,15 @@
+console.log("2"==2);//true
+console.log(null==0);//false
+console.log(null>=0);//true
+
+//all operator work diffrent
+
+console.log("2"===2);// false 
+
+console.log(undefined==0);//false
+console.log(undefined>=0);//false
+
+
+console.log(NaN==0);//false
+
+
