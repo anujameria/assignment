d364e37163ecdf362b42616c20e76b308fb110ca
anuj ameria <sainanuj28@gmail.com>
2024-11-28 11:30:00 +0530

    dataTypes

---
+++ DataTypes/dataTypes.js
@@ -0,0 +1,15 @@
+//types of datatype
+
+
+//number
+//bigint
+//string
+//null
+//undefined
+//object
+
+console.log(typeof"anuj");//string
+console.log(typeof(null));//object
+console.log(typeof(undefined));//undefined
+
+
--- Variable/variable.js
+++ Variable/variable.js
@@ -1,6 +1,7 @@
 let a = 50;
 const b =40;
 // let value change but const not change after declaretion
+// let ek function and block scope variable hai
 
 console.log(a);
 console.log(b);
