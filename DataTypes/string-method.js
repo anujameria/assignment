85086ab55e05987b6f8e59d35c408c53373684b5
anuj ameria <sainanuj28@gmail.com>
2024-11-29 12:21:35 +0530

    datatypes

---
+++ DataTypes/string-method.js
@@ -0,0 +1,13 @@
+let name="    anuj    "
+console.log(name.charAt(2));//u
+
+
+console.log(name.toUpperCase());//    ANUJ
+
+console.log(name.valueOf());//    anuj
+
+console.log(name.trim());//anuj
+
+console.log(name.indexOf("a"));//4
+
+
