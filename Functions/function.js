d6b96d18cb2fce4ec861a5bcc31c2e25801efad3
anuj ameria <sainanuj28@gmail.com>
2024-12-02 16:48:49 +0530

    function

---
+++ Functions/function.js
@@ -0,0 +1,24 @@
+function sum(a,b) {
+    return (`the sum of a and b is ${a+b}`);
+    
+}
+
+const result = sum(5,6)
+//console.log(`result:${result}`);
+
+function intro(username) {
+    return (`hello ${username} welcome in my office`)
+}
+
+//console.log(intro("Anuj Sain"));
+
+
+function cartSum(...cost) {
+        return cost
+    }
+    
+console.log(cartSum(200,300,500));
+
+
+
+
