11c69b36898c5c75dbc56c8728bbec63cc5ebddf
anujameria <sainanuj28@gmail.com>
2024-12-28 19:54:26 +0530

    loops

--- loops.js
+++ loops.js
@@ -4,14 +4,24 @@
     
 // }
 
-myArry=['anuj','sain','ameria']
+// myArry=['anuj','sain','ameria']
 
-for (const key in myArry) {
-    console.log(key);// return key 
+// for (const key in myArry) {
+//     console.log(key);// return key 
     
-}
+// }
 
-for (const element of myArry) {
-    console.log(element);//return element
+// for (const element of myArry) {
+//     console.log(element);//return element
     
+// }
+index=1;
+// do {
+//     console.log(index);//without check condition once time run code  
+//     index++
+// } while (index>10);
+
+while (index<=10) {
+    console.log(index);
+    index++;
 }
