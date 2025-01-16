9c35e1762dcffc88a843c34df723319a6856696a
anujameria <sainanuj28@gmail.com>
2025-01-16 15:15:24 +0530

    first commit

--- Variable/variable.js
+++
@@ -1,8 +0,0 @@
-let a = 50;
-const b =40;
-// let value change but const not change after declaretion
-// let ek function and block scope variable hai jo apne scope se bahar acces nahi hota
-
-console.log(a);
-console.log(b);
-
